888035111feea4c7d1a991ca698e8d8abc65e7b2
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2022-05-12 23:29:12 +0200

    Update 1.c

--- Basic/1.c
+++ Basic/1.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+/*
+My first program in c
+I wish I could like this languages
+*/
+int main(){
+// Where can I write my Hello world text ?
+ printf("Hello, World !!\n");
+ return 0;
+}
