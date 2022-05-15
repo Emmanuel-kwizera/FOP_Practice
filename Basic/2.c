a2c0495f4a0c1c9458a080e505893c321e789dd7
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2022-05-15 21:11:09 +0200

    P-2

--- Basic/2.c
+++ Basic/2.c
@@ -3,12 +3,14 @@ int main() {
 
     int number1, number2, sum;
     
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
+    printf("Enter the first integer: ");
+    scanf("%d", &number1);
+    printf("Enter the second integer: ");
+    scanf("%d", &number2);
 
     // calculating sum
     sum = number1 + number2;      
     
-    printf("the sum of %d and %d is %d", number1, number2, sum);
+    printf("The sum of %d and %d is %d", number1, number2, sum);
     return 0;
 }
