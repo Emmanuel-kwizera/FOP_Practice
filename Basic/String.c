4f0221575ae7729c2f10ad2d341e1159044aeac0
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2022-05-17 20:52:51 +0200

    Update String.c

--- Basic/String.c
+++ Basic/String.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+void displayString(char str[]);
+int main(){
+ char str[50];
+ printf("Enter string: ");
+ gets(str); 
+ displayString(str); // Passing string to a function. 
+ return 0;
+}
+void displayString(char str[])
+{
+ printf("The passed String was: ");
+ puts(str);
+}
