83161c98273cfd4f90a81d390595013c007d2f98
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2022-05-22 23:18:04 +0200

    Update variables.c

--- Basic/variables.c
+++ Basic/variables.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-int main()
-{
- int var = 5;
- printf("Value: %d\n", var);
- printf("Address: %u", &var); //Notice, the ampersand(&) before var.
- return 0;
+
+int main() {
+  int
+  var = 5;
+  printf("Value: %d\n",
+    var);
+  printf("Address: %u", &
+    var); //Notice, the ampersand(&) before var.
+  return 0;
 }
