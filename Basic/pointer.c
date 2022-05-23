eb9e58252268189006e2c0518e3e334d18ef7df6
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2022-05-23 22:28:48 +0200

    Update pointer.c

--- Basic/pointer.c
+++ Basic/pointer.c
@@ -1,1 +1,24 @@
-
+#include <stdio.h>
+int main()
+{
+ int * pc, c;
+ 
+ c = 22;
+ printf("Address of c: %u\n", &c);
+ printf("Value of c: %d\n\n", c);
+ 
+ pc = &c;
+ printf("Address of pointer pc: %u\n", pc);
+ printf("Content of pointer pc: %d\n\n", *pc);
+ 
+ c = 11;
+ printf("Address of pointer pc: %u\n", pc);
+ printf("Content of pointer pc: %d\n\n", *pc);
+ 
+ 
+// a pointer can change the valu of a variable
+ *pc = 2;
+ printf("Address of c: %u\n", &c);
+ printf("Value of c: %d\n\n", c);
+ return 0;
+}
